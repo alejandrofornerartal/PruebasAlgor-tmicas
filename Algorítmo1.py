87f21590e1fe96f24d97761c47691f672f70f674
alejandrofornerartal <84869804+alejandrofornerartal@users.noreply.github.com>
2023-02-15 13:55:27 +0100

    Add files via upload

---
+++ Algorítmo1.py
@@ -0,0 +1,12 @@
+print("Prueba algorítmica 1")
+print(" ")
+
+leernumero = 0
+
+leernumero = int(input("Indique un número: "))
+
+if (leernumero % 2) == 0:
+  print(leernumero, " es un número par")
+else:
+  print(leernumero, " es un número impar")
+
